b83c0b8c4bb803b1c41689541de42ca028452f95
Andrew Donnellan <andrew@donnellan.id.au>
2020-09-17 13:09:52 +1000

    hmmmmmmmmm
    Signed-off-by: Andrew Donnellan <andrew@donnellan.id.au>

--- package/syzkaller/syzkaller.mk
+++ package/syzkaller/syzkaller.mk
@@ -9,4 +9,16 @@ SYZKALLER_SITE = $(call github,google,syzkaller,$(SYZKALLER_VERSION))
 SYZKALLER_LICENSE = Apache-2.0
 SYZKALLER_LICENSE_FILES = LICENSE
 
-$(eval $(golang-package))
+# We don't use the golang-package helper, because syzkaller supplies its own
+# Makefile and does some very strange things in the build
+
+# TODO: Need to use the internal go toolchain rather than whatever happens to be running on the host...
+
+# Map Buildroot architecture names to golang architecture names
+
+define SYZKALLER_BUILD_CMDS
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) HOSTARCH=$(GO_GOARCH) -C $(@D) all
+endef
+
+$(info $(generic-package))
+$(eval $(generic-package))
